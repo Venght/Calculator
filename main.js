46eb4161d07734f00e7fb89584d7ed9fcd6582f9
Venght <bvenght@gmail.com>
2025-02-10 19:15:57 +0800

    setup the buttons in html

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 let num1 = parseInt(prompt("Enter the first number?"));
 let num2 = parseInt(prompt("Enter the second number?"));
 
+const button = document.querySelectorAll(".btn");
+
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
+
+function operate(operator, a, b) {
+
+    if(operator == "+") {
+        return a + b;
+    }
+    else if(operator == "-"){
+        return a - b;
+    }
+    else if(operator == "*"){
+        return a * b;
+    }
+    else if(operator == "/") {
+        return a / b;
+    }
+    else {
+        return null;
+    }
+}
+
 function add(a, b) {
  return a + b;
 }
@@ -23,6 +48,8 @@ function getNumber() {
     let num2 = parseInt(prompt("Enter the second number?"));
     return num1, num2;
 }
+
+
 
 console.log("Addition:", add(num1, num2));
 console.log("Subtraction:", subtract(num1, num2));
